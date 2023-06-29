1bee08992fbb01482c6e285e4781b0745fc37d1e
DeepSOIC <vv.titov@gmail.com>
2023-06-29 17:13:48 +0200

    clean up
    * remove dead code
    * minor style changes

--- src/main.rs
+++ src/main.rs
@@ -6,80 +6,52 @@ use simplebgc::{self, ParamsQuery};
 use anyhow::{Context as _, Result as _};
 use futures::{StreamExt as _, SinkExt as _};
 
-enum FromGimbalMsg {
-    Config(simplebgc::Params3Data), //FIXME: use Ext3
-    EncoderData((f64, f64)), //yaw, pitch
-    Other(simplebgc::IncomingCommand),
-}
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
+    let baud_rate = 115_200;
+    let port = "COM14";
 
-    let (incoming_messages_prod, incoming_messages_cons) = 
-        mpsc::channel::<FromGimbalMsg>();
-    {
+    let serial_device = tokio_serial::new(port, baud_rate)
+        .open_native_async()
+        .with_context(|| format!("Failed to open PWM serial device {}", port))?;
 
-        let baud_rate = 115_200;
-        let port = "COM14";
+    let framed = tokio_util::codec::Framed::new(serial_device, simplebgc::V2Codec);
+    
+    let (mut messages_tx, mut messages_rx) = framed.split();
 
-        let serial_device = tokio_serial::new(port, baud_rate)
-            .open_native_async()
-            .with_context(|| format!("Failed to open PWM serial device {}", port))?;
+    use tokio::time::timeout;
+    use std::time::Duration;
+    let (mut messages_tx, mut messages_rx, offset_yaw, offset_pitch) = 
+    timeout(Duration::from_millis(1000), async move {
+        messages_tx.send(
+            simplebgc::OutgoingCommand::ReadParamsExt(ParamsQuery{profile_id: 0})
+        ).await.unwrap();
 
-        // let mut rx_task = tokio::spawn(async move {
-        //     gimbal_rx_loop(
-        //         incoming_messages_prod,
-        //         serial_device,
-        //     )
-        //     .await;
-        // });
+        loop { 
+            let msg = messages_rx.next().await.unwrap().unwrap();
+            match msg {
+                simplebgc::IncomingCommand::ReadParamsExt(cmd) => {
+                    let (y,p,r) = (cmd.encoder_offset.yaw, cmd.encoder_offset.pitch, cmd.encoder_offset.roll);
+                    println!(" raw offsets y-p-r: {y}, {p}, {r}");
+                    let offset_yaw = (cmd.encoder_offset.yaw as f64) / ((1 << 14) as f64);
+                    let offset_pitch = (cmd.encoder_offset.pitch as f64) / ((1 << 14) as f64);
 
-        let framed = tokio_util::codec::Framed::new(serial_device, simplebgc::V2Codec);
-        
-        let (mut messages_tx, mut messages_rx) = framed.split();
-
-        use tokio::time::timeout;
-        use std::time::Duration;
-        let (mut messages_tx, mut messages_rx, offset_yaw, offset_pitch) = 
-        timeout(Duration::from_millis(1000), async move {
-            messages_tx.send(
-                simplebgc::OutgoingCommand::ReadParamsExt(ParamsQuery{profile_id: 0})
-            ).await.unwrap();
-
-            if false {
-                return  Err("");
-            };
-            loop { 
-                let msg = messages_rx.next().await.unwrap().unwrap();
-                match msg {
-                    simplebgc::IncomingCommand::ReadParamsExt(cmd) => {
-                        let (y,p,r) = (cmd.encoder_offset.yaw, cmd.encoder_offset.pitch, cmd.encoder_offset.roll);
-                        println!(" raw offsets y-p-r: {y}, {p}, {r}");
-                        let offset_yaw = (cmd.encoder_offset.yaw as f64) / ((1 << 14) as f64);
-                        let offset_pitch = (cmd.encoder_offset.pitch as f64) / ((1 << 14) as f64);
-
-                        break Ok((messages_tx, messages_rx, offset_yaw, offset_pitch));
-                    }
-                    _ => {
-                        println!("got some other message while waiting for encoder offsets");
-                    }
+                    return Ok((messages_tx, messages_rx, offset_yaw, offset_pitch));
+                }
+                _ => {
+                    println!("got some other message while waiting for encoder offsets");
                 }
             }
+        }
 
-            
-        }).await.expect("no response from gimbal (timeout)").expect("data not received");
+        //ugly hack to force concrete error type, to make it compile
+        if false {
+            return  Err(()); 
+        };
+    }).await.expect("no response from gimbal (timeout)").expect("data not received");
 
-        println!("offsets: {offset_yaw}, {offset_pitch}");
-    };
-    Ok(())
-}
-
-async fn gimbal_rx_loop(
-    mut incoming_messages_prod: mpsc::Sender<FromGimbalMsg>,
-    serial_device: tokio_serial::SerialStream,
-) -> Result<(), Box<dyn std::error::Error>> {
+    println!("offsets: {offset_yaw}, {offset_pitch}");
 
     Ok(())
 }
